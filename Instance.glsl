954c89b869d9ca33c09c06df0b58d8d31ec6cbf5
lijenicol <elijah_nicol@hotmail.com>
2022-11-16 16:39:45 +1100

    Fixed some src code bugs

--- Instance.glsl
+++ Instance.glsl
@@ -39,7 +39,7 @@ uniform vec3 LightDirection=normalize(vec3(-1,0,0));
 uniform vec3 LightColor=vec3(1.0, 1.0, 1.0);
 uniform vec3 DiffuseColor=vec3(0.0,0.5,0.9);
 
-out vec3 finalColor;
+out vec4 finalColor;
 
 ////////////////////////////////////////
 // Fragment shader
@@ -50,7 +50,7 @@ void main() {
 	vec3 irradiance= vec3(0.3,0.3,0.3) * DiffuseColor + DiffuseColor * LightColor * max(0,dot(LightDirection,normalize(fragNormal)));
 	
 	// Gamma correction
-	gl_FragColor=vec4(irradiance,1);
+	finalColor=vec4(irradiance,1);
 }
 
 #endif
